b81cbaf94e90aba4e4d1448b3c7ca1ec86d34c36
Jensy Figueroa <jensy1418@gmail.com>
2023-06-04 19:14:58 -0400

    added scroll

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,39 @@ import { Routes, Route } from 'react-router-dom'
 import Header from './components/page/header/Header.jsx'
 import Main from './components/page/main/Main'
 import Footer from './components/page/footer/Footer'
+import './index.css'
+import { useEffect } from 'react'
 
 function App() {
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollUp = document.getElementById("scrollUp");
+
+      if (window.scrollY >= 350) {
+        scrollUp.classList.add("showScroll");
+      } else {
+        scrollUp.classList.remove("showScroll");
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
   return (
     <>
       <Header />
       <Routes>
-        <Route path='/' element={<Main/>}/>
+        <Route path='/' element={<Main />} />
       </Routes>
-      <Footer/>
+      <Footer />
+
+      <a href="#home" className="scrollUp" id="scrollUp">
+        <i className="fa-solid fa-arrow-up"></i>
+      </a>
     </>
   )
 }
