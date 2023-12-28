5e3b672bea9c81a0c494d8b8972ed2bcc7e54fa9
Jensy Figueroa <jensy1418@gmail.com>
2023-12-28 13:44:32 -0500

    the vercel json file was added 3

--- src/components/cv/DownloadCV.jsx
+++ src/components/cv/DownloadCV.jsx
@@ -22,7 +22,7 @@ export default function BasicMenu() {
     
     if (lenguangeCV === 'Spanish') {
       setLenguangeCV('English')
-      window.open('/public/cv/CV-Spanish.pdf', '_blank');
+      window.open('/cv/CV-Spanish.pdf', '_blank');
     }else{
       setLenguangeCV('Spanish')
       window.open('/src/assets/cv/CV-English.pdf', '_blank');
