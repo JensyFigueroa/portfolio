ac90a7b345f42329c5a0d6ab906b94ceaec87bd8
Jensy Figueroa <jensy1418@gmail.com>
2023-12-28 13:46:42 -0500

    the vercel json file was added 4

--- src/components/cv/DownloadCV.jsx
+++ src/components/cv/DownloadCV.jsx
@@ -25,7 +25,7 @@ export default function BasicMenu() {
       window.open('/cv/CV-Spanish.pdf', '_blank');
     }else{
       setLenguangeCV('Spanish')
-      window.open('/src/assets/cv/CV-English.pdf', '_blank');
+      window.open('assets/cv/CV-English.pdf', '_blank');
     }    
 
   };
